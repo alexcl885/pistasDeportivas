e7afa543f36adb6812be35e1a9c59477ab3a52ce
Alejandro Copado Lopez <alexcopado2005@gmail.com>
2024-12-21 18:46:14 +0100

    vuelta al principio

--- src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiSec.java
+++ src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiSec.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -46,7 +47,7 @@ public class ConfiSec {
                     "/logout", "/acerca", "/denegado")
                     .permitAll()
                 .requestMatchers(
-                    "/horario/**", "/instalacion/**", "/reservas/**")
+                    "/horario/**", "/instalacion/**")
                     .hasAuthority("ADMIN")
                 .requestMatchers(
                     "/mis-datos/**", "/mis-datos/*/**" )
