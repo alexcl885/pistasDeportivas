6a00294a522f8b79921b8c82a4f0fd239e065055
Alejandro Copado Lopez <alexcopado2005@gmail.com>
2025-01-27 12:37:03 +0100

    cambios realizados merge dani y alex 2

--- src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiSec.java
+++ src/main/java/com/iesvdc/acceso/pistasdeportivas/configuracion/ConfiSec.java
@@ -47,7 +47,7 @@ public class ConfiSec {
                     "/logout", "/acerca", "/denegado")
                     .permitAll()
                 .requestMatchers(
-                    "/horario/**", "/instalacion/**")
+                    "/horario/**", "/instalacion/**", "/reservas/**")
                     .hasAuthority("ADMIN")
                 .requestMatchers(
                     "/mis-datos/**", "/mis-datos/*/**")
